421109abf3e1d62b6eb2458c52bc110875365a22
Rony Cuzco <ronald.cuzco@gmail.com>
2018-12-08 11:11:43 +0100

    cambios en editor config

--- src/app/components/heroes/heroe.component.ts
+++ src/app/components/heroes/heroe.component.ts
@@ -5,34 +5,34 @@ import { Heroe } from 'src/app/interfaces/heroe.interface';
 import { HeroesService } from 'src/app/services/heroes.service';
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html',
+    styleUrls: ['./heroe.component.css']
 })
 export class HeroeComponent implements OnInit
 {
 
-  heroe: Heroe =
+    heroe: Heroe =
+        {
+            bio: "",
+            casa: "Marval",
+            nombre: ""
+        };
+
+    constructor(private _heroeService: HeroesService) { }
+
+    ngOnInit()
     {
-      bio: "",
-      casa: "Marval",
-      nombre: ""
-    };
+    }
 
-  constructor(private _heroeService:HeroesService) { }
-
-  ngOnInit()
-  {
-  }
-
-  guardar()
-  {
-    console.log(this.heroe);
-    this._heroeService.nuevoHeroe(this.heroe)
-      .subscribe(responseData =>
-      {
-        console.log("response Data", responseData);
-      });
-  }
+    guardar()
+    {
+        console.log(this.heroe);
+        this._heroeService.nuevoHeroe(this.heroe)
+            .subscribe(responseData =>
+            {
+                console.log("response Data", responseData);
+            });
+    }
 
 }
